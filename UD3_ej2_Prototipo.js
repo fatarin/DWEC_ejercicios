d456820176b2a721b880e612b5375c1341582066
fatarin <fatarin@gmail.com>
2023-11-08 19:12:40 +0100

    require solo funciones necesarias en Prototipo

--- UD3_ej2_Prototipo.js
+++ UD3_ej2_Prototipo.js
@@ -1,23 +1,23 @@
-const addmodule = require ('./UD3_ej2_modulos.js');
+const {aleatorio, estado} = require ('./UD3_ej2_modulos.js');
 
 let prototipo = {
     hambre: 15,
     cansancio: 15,
     felicidad: 15,
     comer: function come(){
-        let valor = addmodule.aleatorio();
+        let valor = aleatorio();
         this.hambre -= valor;
     },
     dormir: function duerme(){
-        let valor = addmodule.aleatorio();
+        let valor = aleatorio();
         this.cansancio -= valor;
     },
     jugar: function juega(){
-        let valor = addmodule.aleatorio();
+        let valor = aleatorio();
         this.felicidad += valor;
     },
     pasoTiempo: function pasoTiempo(tiempo){
-        let valor = addmodule.aleatorio();
+        let valor = aleatorio();
         this.felicidad -= valor;
         this.hambre += valor;
         this.cansancio += valor;
@@ -25,19 +25,21 @@ let prototipo = {
 }
 
 let lola = Object.create(prototipo);
+
+//pruebas
 console.log("Estado inicial:" );
-addmodule.estado(lola);
+estado(lola);
 console.log("Interacción: come, juega");
 lola.comer();
 lola.jugar();
 console.log("Estado: \n")
-addmodule.estado(lola);
+estado(lola);
 console.log("Paso del tiempo");
 lola.pasoTiempo();
 console.log("Estado: \n")
-addmodule.estado(lola);
+estado(lola);
 console.log("Interacción: duerme");
 lola.dormir();
 console.log("Estado: \n")
-addmodule.estado(lola);
+estado(lola);
 
